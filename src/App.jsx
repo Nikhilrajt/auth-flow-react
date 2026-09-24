ea88aeaa6f921a9d0d83399a0ce821f8957b81b3
Nikhilrajt <nehalnikhil25@gmail.com>
2026-09-24 23:07:06 +0530

    Configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return <div>
-  <RegisterPage/>
-  </div>
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/register" element={<RegisterPage />}/>
+  <Route path="/login" element={<h1>Login Page</h1>}/>
+  <Route path="/dashboard" element={<h1>Dashboard Page</h1>} />
+  <Route path="/" element={<Navigate to="/login" replace />} />
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
